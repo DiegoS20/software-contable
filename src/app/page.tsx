313913fa3db38b7e865a2eb3112866998274a3a4
Diego Saravia <saravia__2000@hotmail.com>
2023-11-18 09:00:55 -0600

    feat: daily book setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+
 import styles from "./page.module.css";
+import DailyBook from "@/components/DailyBook";
 
 export default function Home() {
+  const [page, setPage] = useState(0);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box
+        display="flex"
+        flexDirection="column"
+        sx={{ width: "100%", height: "100vh" }}
+      >
+        <div style={{ flex: "1" }}>{page == 0 && <DailyBook />}</div>
+        <BottomNavigation
+          showLabels
+          value={page}
+          onChange={(_, newValue) => {
+            setPage(newValue);
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <BottomNavigationAction
+            label="Libro diario"
+            icon={<LibraryBooksIcon />}
+          />
+          <BottomNavigationAction
+            label="Balance de comprobación"
+            icon={<AccountBalanceWalletIcon />}
+          />
+          <BottomNavigationAction label="Kardex" icon={<CreditCardIcon />} />
+        </BottomNavigation>
+      </Box>
     </main>
   );
 }
---
+++ src/components/DailyBook/index.tsx
@@ -0,0 +1,3 @@
+export default function DailyBook() {
+  return <div>Daily book</div>;
+}
