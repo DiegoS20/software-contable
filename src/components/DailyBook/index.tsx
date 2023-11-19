f709c0937b8a4345ee68769e1d59552b8118aff4
Diego Saravia <saravia__2000@hotmail.com>
2023-11-19 16:27:11 -0600

    feat: using modal in page (incompleted)

--- src/components/DailyBook/index.tsx
+++ src/components/DailyBook/index.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@mui/material";
+import Modal from "../Modal";
+import { useState } from "react";
+
 export default function DailyBook() {
-  return <div>Daily book</div>;
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => setOpenModal(true)}
+      >
+        Agregar asiento
+      </Button>
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <p>Hola</p>
+      </Modal>
+    </div>
+  );
 }
